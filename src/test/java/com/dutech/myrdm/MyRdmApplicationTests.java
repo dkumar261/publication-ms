b6bf7e7ab85b90f0225067accc1281bbccbe8771
Dinesh <krsnavicky@gmail.com>
2019-05-10 13:27:00 +0530

    Test file adding.

---
+++ src/test/java/com/dutech/myrdm/MyRdmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dutech.myrdm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyRdmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
